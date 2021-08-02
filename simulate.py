6b904e9ab704fb514d6a827fbebd5a09f23bc06e
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2021-08-02 08:01:11 -0400

    reformat, added comments

--- simulate.py
+++ simulate.py
@@ -3,93 +3,132 @@ import numpy as np
 import pandas as pd
 import pylab as plt
 
-def gen_bursts(mu_params, sigma_params, mu_dist='uniform', sigma_dist='uniform', 
-               N=10000, alpha=-1.5, E_min_log=37, E_max_log=39, name='', save=True):
-    
-    if mu_dist == 'uniform':
+
+def gen_bursts(
+    mu_params,
+    sigma_params,
+    mu_dist="uniform",
+    sigma_dist="uniform",
+    N=10000,
+    alpha=-1.5,
+    E_min_log=37,
+    E_max_log=39,
+    name="",
+    save=True,
+):
+    """
+    Generates a sample of FRBs and returns a dataframe.
+    Each row corresponds to an FRB. Spectra is modeled
+    using Gaussian, so each row consists of mu_f, sigma_f
+    and energy of that FRB.
+
+    mu_params: list containing the parameter of mu_f distribution
+    sigma_params: list containing the parameter of sigma_f distribution
+    mu_dist: distribution of mu_f
+    sigma_dist: distribution of sigma_f
+    N: Number of FRBs to generate
+    alpha: Energy distribution slope
+    E_min_log: Minimum energy cutoff in units of log10(E ergs)
+    E_max_log: Maximum energy cutoff in units of log10(E ergs)
+    name: Name of the output file
+    save: Save the dataframe to a csv
+    """
+    if mu_dist == "uniform":
         low = mu_params[0]
         high = mu_params[1]
-        mu_f = np.random.uniform(low=low, 
-                                 high=high, 
-                                 size=N) # MHz
-    elif mu_dist == 'gauss' or mu_dist == 'norm':
+        mu_f = np.random.uniform(low=low, high=high, size=N)  # MHz
+    elif mu_dist == "gauss" or mu_dist == "norm":
         loc = mu_params[0]
         scale = mu_params[1]
         mu_f = np.random.normal(loc=loc, scale=scale, size=N)  # MHz
     else:
         raise ValueError
-        
-    if sigma_dist == 'uniform':
-        sig_f = np.random.uniform(low=sigma_params[0], 
-                                  high = sigma_params[1], 
-                                  size=N) # MHz
-    elif sigma_dist == 'gauss' or sigma_dist == 'norm':
+
+    if sigma_dist == "uniform":
+        sig_f = np.random.uniform(
+            low=sigma_params[0], high=sigma_params[1], size=N
+        )  # MHz
+    elif sigma_dist == "gauss" or sigma_dist == "norm":
         loc = sigma_params[0]
         scale = sigma_params[1]
         sig_f = np.random.normal(loc=loc, scale=scale, size=N)  # MHz
     else:
         raise ValueError
-    
+
     m = sig_f > 0
     N_new = m.sum()
 
     sig_f = sig_f[m]
-    mu_f = mu_f[m]        
-        
+    mu_f = mu_f[m]
+
     Es = []
     for i in range(N_new):
         Es.append(sample_from_powerlaw(E_min_log, E_max_log, alpha))
     Es = np.array(Es)
-    
+
     vals = {}
-    vals['in_mu_f'] = mu_f
-    vals['in_sig_f'] = sig_f
-    vals['E'] = Es
+    vals["in_mu_f"] = mu_f
+    vals["in_sig_f"] = sig_f
+    vals["E"] = Es
     df_vals = pd.DataFrame.from_dict(vals)
-    
-    mu_params_n = f'{mu_params[0]}_{mu_params[1]}'
-    sigma_params_n = f'{sigma_params[0]}_{sigma_params[1]}'
-    
+
+    mu_params_n = f"{mu_params[0]}_{mu_params[1]}"
+    sigma_params_n = f"{sigma_params[0]}_{sigma_params[1]}"
+
     if len(name):
-        name += '_'
-    name += f'data_mu_{mu_dist}_{mu_params_n}_sig_{sigma_dist}_{sigma_params_n}_'
-    name += f'alpha_{alpha}_Elog_{E_min_log}_{E_max_log}.csv'
+        name += "_"
+    name += f"data_mu_{mu_dist}_{mu_params_n}_sig_{sigma_dist}_{sigma_params_n}_"
+    name += f"alpha_{alpha}_Elog_{E_min_log}_{E_max_log}.csv"
     if save:
         df_vals.to_csv(name)
     return df_vals, name
 
 
-def run_search(bursts, fstart, fend, fluence_threshold, 
-               in_band_sig=1, ret='all', distance=972):
+def run_search(
+    bursts, fstart, fend, fluence_threshold, in_band_sig=1, ret="all", distance=972
+):
     """
-    fluence_threshold: in Jy ms 
-    fstart, fend: MHz
-    bursts: dataframe
-    distance: Mpc
+    Runs a search on the bursts given in the input dataframe.
+    Integrates the spectra within bandwidth bounds to determine
+    the fraction of signal within the observing band. Checks
+    if that value crosses the fluence threshold to determine
+    if the burst is detected.
+    Estimates the energy of the burst
+    from snr-fluence and approx bandwidth of the burst. Here,
+    the bandwidth is the burst signal seen within the observing band.
+    It also estimates fit-energy using fit-fluence and actual
+    bandwidth of the burst.
+    Also, returns the energies of in-band bursts.
+
+    bursts: dataframe with burst information
+    fstart, fend (MHz): Observing band
+    fluence_threshold: Jy ms
+    in_band_sig: sigma to use to determine in-band bursts
+    ret: To select the energies to return
+    distance: (Mpc) Distance to the FRB
+
     """
 
-    mu_f = bursts['in_mu_f']
-    sig_f = bursts['in_sig_f']
-    Es = bursts['E']
+    mu_f = bursts["in_mu_f"]
+    sig_f = bursts["in_sig_f"]
+    Es = bursts["E"]
     spectra_frac = gauss_integral(fstart, fend, mu_f, sig_f)
-    
-    S =  energy_to_fluence(Es, sig_f, distance) # Jy s
+
+    S = energy_to_fluence(Es, sig_f, distance)  # Jy s
     assert len(np.where(sig_f < 0)[0]) == 0
 
-    # IS THIS CORRECT?
-    S_seen = S * spectra_frac # fluence within the observable band
-    bursts['in_S'] = S
-    bursts['snr_S'] = S_seen
-    fluence_threshold_jys = fluence_threshold/1000
-    detected_df = bursts[bursts['snr_S'] > fluence_threshold_jys]
+    S_seen = S * spectra_frac  # fluence within the observable band
+    bursts["in_S"] = S
+    bursts["snr_S"] = S_seen
+    fluence_threshold_jys = fluence_threshold / 1000
+    detected_df = bursts[bursts["snr_S"] > fluence_threshold_jys]
     # ----------
-    
-    original_E = np.array(bursts['E'])
-    
-    # detected_snr_E = np.array(detected_df['snr_S'] * 2.355 * detected_df['in_sig_f'])
-    u = np.array(detected_df['in_mu_f'] + 1*detected_df['in_sig_f'])
-    l = np.array(detected_df['in_mu_f'] - 1*detected_df['in_sig_f'])
-    
+
+    original_E = np.array(bursts["E"])
+
+    u = np.array(detected_df["in_mu_f"] + 1 * detected_df["in_sig_f"])
+    l = np.array(detected_df["in_mu_f"] - 1 * detected_df["in_sig_f"])
+
     uu = u >= fend
     ul = u <= fstart
     lu = l >= fend
@@ -108,82 +147,99 @@ def run_search(bursts, fstart, fend, fluence_threshold,
 
     u[uu & lu] = fend
     # l[uu & lu] = l[uu & lu]
-    
+
     freq_width = u - l
     assert len(np.where(freq_width < 0)[0]) == 0
-    detected_snr_E = np.array(fluence_to_energy(detected_df['snr_S'], freq_width, distance))
-    detected_fit_E = np.array(detected_df['E'])
-    
-    upper = (detected_df['in_mu_f'] + in_band_sig*detected_df['in_sig_f'] < fend)
-    lower = (detected_df['in_mu_f'] - in_band_sig*detected_df['in_sig_f'] > fstart)
+    detected_snr_E = np.array(
+        fluence_to_energy(detected_df["snr_S"], freq_width, distance)
+    )
+    detected_fit_E = np.array(detected_df["E"])
+
+    upper = detected_df["in_mu_f"] + in_band_sig * detected_df["in_sig_f"] < fend
+    lower = detected_df["in_mu_f"] - in_band_sig * detected_df["in_sig_f"] > fstart
     detected_in_band_df = detected_df[upper & lower]
-    detected_in_band_E = detected_in_band_df['E']
-    
-    if ret=='all':
+    detected_in_band_E = detected_in_band_df["E"]
+
+    if ret == "all":
         Es = {}
-        Es['original_E'] = original_E
-        Es['detected_snr_E'] = detected_snr_E
-        Es['detected_fit_E'] = detected_fit_E
-        Es['detected_in_band_E'] = detected_in_band_E
+        Es["original_E"] = original_E
+        Es["detected_snr_E"] = detected_snr_E
+        Es["detected_fit_E"] = detected_fit_E
+        Es["detected_in_band_E"] = detected_in_band_E
         return detected_df, detected_in_band_df, Es
-    elif ret=='snr_E':
+    elif ret == "snr_E":
         return detected_snr_E
-    elif ret=='in_band_E':
+    elif ret == "in_band_E":
         return detected_in_band_E
     else:
         raise ValueError
-        
-        
+
+
 def analyse_and_plot(bursts, detected, Es):
-    
-    qs = np.quantile(bursts['in_mu_f'], [0.16, 0.5, 0.84])
-    print(f'Injected mu_f is: {qs[1]:.3f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}')
+    "Plot the histogram of energies and print statistics of injected and recovered parameters"
 
-    qs = np.quantile(detected['in_mu_f'], [0.16, 0.5, 0.84])
-    print(f'Recovered mu_f is: {qs[1]:.3f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}')
-    
-    qs = np.quantile(bursts['in_sig_f'], [0.16, 0.5, 0.84])
-    print(f'Injected sig_f is: {qs[1]:.2f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}')
+    qs = np.quantile(bursts["in_mu_f"], [0.16, 0.5, 0.84])
+    print(f"Injected mu_f is: {qs[1]:.3f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}")
 
-    qs = np.quantile(detected['in_sig_f'], [0.16, 0.5, 0.84])
-    print(f'Recovered sig_f is: {qs[1]:.2f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}')
-    
+    qs = np.quantile(detected["in_mu_f"], [0.16, 0.5, 0.84])
+    print(f"Recovered mu_f is: {qs[1]:.3f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}")
+
+    qs = np.quantile(bursts["in_sig_f"], [0.16, 0.5, 0.84])
+    print(f"Injected sig_f is: {qs[1]:.2f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}")
+
+    qs = np.quantile(detected["in_sig_f"], [0.16, 0.5, 0.84])
+    print(
+        f"Recovered sig_f is: {qs[1]:.2f}+{(qs[2] - qs[1]):.2f}-{(qs[1] - qs[0]):.2f}"
+    )
+
     fig, axes = plt.subplots(1, 3, sharex=False, sharey=False, figsize=(12, 4))
-    v, bins, _ = axes[0].hist(bursts['in_mu_f'], alpha=0.5, density=True, label='in')
-    axes[0].hist(detected['in_mu_f'], alpha=0.5, density=True, label='detected', bins=bins)
-    axes[0].set_title('mu_f')
+    v, bins, _ = axes[0].hist(bursts["in_mu_f"], alpha=0.5, density=True, label="in")
+    axes[0].hist(
+        detected["in_mu_f"], alpha=0.5, density=True, label="detected", bins=bins
+    )
+    axes[0].set_title("mu_f")
     axes[0].legend()
-    
-    original_E = Es['original_E']
-    detected_snr_E = Es['detected_snr_E']
-    detected_fit_E = Es['detected_fit_E']
-    detected_in_band_E = Es['detected_in_band_E']
 
-    v, bins, _ = axes[1].hist(bursts['in_sig_f'], alpha=0.5, density=True, bins=30, label='in')
-    axes[1].hist(detected['in_sig_f'], alpha=0.5, density=True, label='detected', bins=bins)
-    axes[1].set_title('sig_f')
+    original_E = Es["original_E"]
+    detected_snr_E = Es["detected_snr_E"]
+    detected_fit_E = Es["detected_fit_E"]
+    detected_in_band_E = Es["detected_in_band_E"]
+
+    v, bins, _ = axes[1].hist(
+        bursts["in_sig_f"], alpha=0.5, density=True, bins=30, label="in"
+    )
+    axes[1].hist(
+        detected["in_sig_f"], alpha=0.5, density=True, label="detected", bins=bins
+    )
+    axes[1].set_title("sig_f")
     axes[1].legend()
 
-    axes[2].scatter(detected['in_S'], detected['snr_S'])
-    axes[2].plot(detected['in_S'], detected['in_S'], c='k')
-    axes[2].set_xscale('log')
-    axes[2].set_yscale('log')
-    axes[2].set_xlabel('in S')
-    axes[2].set_ylabel('snr S')
-    
+    axes[2].scatter(detected["in_S"], detected["snr_S"])
+    axes[2].plot(detected["in_S"], detected["in_S"], c="k")
+    axes[2].set_xscale("log")
+    axes[2].set_yscale("log")
+    axes[2].set_xlabel("in S")
+    axes[2].set_ylabel("snr S")
+
     fig, axes = plt.subplots(1, 3, sharex=False, sharey=False, figsize=(12, 4))
     axes[0].hist(np.log10(original_E), bins=30, density=True)
-    axes[0].set_yscale('log')
-    axes[0].set_title('original_E')
-    
-    axes[1].hist(np.log10(detected_snr_E), bins=30, density=True)
-    axes[1].set_yscale('log')
-    axes[1].set_title('detected_snr_E')
+    axes[0].set_yscale("log")
+    axes[0].set_title("original_E")
 
-    axes[2].hist(np.log10(detected_fit_E), bins=30, density=True, alpha=0.5,
-                 label='fit_E')
-    axes[2].hist(np.log10(detected_in_band_E), bins=30, density=True, alpha=0.5, 
-                 label='in_band_E')
-    axes[2].set_yscale('log')
-    axes[2].set_title('fit and in-band E')
+    axes[1].hist(np.log10(detected_snr_E), bins=30, density=True)
+    axes[1].set_yscale("log")
+    axes[1].set_title("detected_snr_E")
+
+    axes[2].hist(
+        np.log10(detected_fit_E), bins=30, density=True, alpha=0.5, label="fit_E"
+    )
+    axes[2].hist(
+        np.log10(detected_in_band_E),
+        bins=30,
+        density=True,
+        alpha=0.5,
+        label="in_band_E",
+    )
+    axes[2].set_yscale("log")
+    axes[2].set_title("fit and in-band E")
     axes[2].legend()
--- utils.py
+++ utils.py
@@ -9,53 +9,73 @@ from scipy.optimize import curve_fit
 def sample_from_powerlaw(logminval, logmaxval, power):
     """Draw a value randomly from the specified power law"""
 
-    logmin = logminval #np.log10(minval)
-    logmax = logmaxval #np.log10(maxval)
+    logmin = logminval  # np.log10(minval)
+    logmax = logmaxval  # np.log10(maxval)
 
     c = -1.0 * logmax * power
-    nmax = 10.0**(power*logmin + c)
+    nmax = 10.0 ** (power * logmin + c)
 
     # slightly worried about inf loops here...
     while True:
         log = np.random.uniform(logmin, logmax)
-        n = 10.0**(power*log + c)
+        n = 10.0 ** (power * log + c)
 
-        if nmax*np.random.random() <= n:
+        if nmax * np.random.random() <= n:
             break
 
-    return 10.0**log
+    return 10.0 ** log
 
 
 def gauss_integral(fstart, fend, mu_f, sig_f):
+    """
+    Integrate a gaussian function within limits
+
+    fstart: start frequency of the band
+    fend: end frequency of the band
+    mu_f: mean of the gaussian
+    sig_f: sigma of the gaussian
+
+    """
     # https://www.wolframalpha.com/input/
     # ?i=integral+of+%281%2F%28sqrt%282*pi%29*sigma%29%29*e%5E%28-
     # %28%28x-mu%29%2F%28sigma%29%29%5E2%2F2%29+dx
-    
-    # Verify if this works!!
-    inp_start = (fstart - mu_f)/(np.sqrt(2*np.pi) * sig_f)
-    inp_end = (fend - mu_f)/(np.sqrt(2*np.pi) * sig_f)
+
+    inp_start = (fstart - mu_f) / (np.sqrt(2 * np.pi) * sig_f)
+    inp_end = (fend - mu_f) / (np.sqrt(2 * np.pi) * sig_f)
     integral = 0.5 * (special.erf(inp_end) - special.erf(inp_start))
     return integral
 
 
 def energy_to_fluence(E, sig_f, distance=972):
-    # E in ergs
-    # sig_f in MHz
-    D_l = distance * 10**6 * 3 * 10**18 # cm 
-    fac = 10**(-23)
-    return E / (2.355*sig_f*10**6) / (4*np.pi*D_l**2) / fac # Jy s 
+    """
+    Convert energy to fluence using burst bandwidth
+    and distance
+
+    E in ergs
+    sig_f in MHz
+    distance in Mpc
+    """
+    D_l = distance * 10 ** 6 * 3 * 10 ** 18  # cm
+    fac = 10 ** (-23)
+    return E / (2.355 * sig_f * 10 ** 6) / (4 * np.pi * D_l ** 2) / fac  # Jy s
 
 
 def fluence_to_energy(S, freq_width, distance=972):
-    # S in Jy s
-    # freq_width in MHz
-    D_l = distance * 10**6 * 3 * 10**18 # cm 
-    fac = 10**(-23)
-    return S * (freq_width*10**6) * (4*np.pi*D_l**2) * fac # ergs
+    """
+    Convert fluence to energy using burst bandwidth
+    and distance
+
+    S in Jy s
+    sig_f in MHz
+    distance in Mpc
+    """
+    D_l = distance * 10 ** 6 * 3 * 10 ** 18  # cm
+    fac = 10 ** (-23)
+    return S * (freq_width * 10 ** 6) * (4 * np.pi * D_l ** 2) * fac  # ergs
 
 
 def single_power_law(x, a, b):
-    return a*x**(b)
+    return a * x ** (b)
 
 
 def fit_single_power_law(Ns, Es):
@@ -74,11 +94,14 @@ def double_power_law(x, amplitude, x_break, alpha_1, alpha_2):
 
 
 def fit_double_power_law(Ns, Es):
-    popt, perr = curve_fit(double_power_law, Es, Ns, 
-                           bounds=([-np.inf, 10**(0), -5, -5],
-                                   [np.inf, 10**(4), 5, 5]))
+    popt, perr = curve_fit(
+        double_power_law,
+        Es,
+        Ns,
+        bounds=([-np.inf, 10 ** (0), -5, -5], [np.inf, 10 ** (4), 5, 5]),
+    )
     slope1 = popt[-2]
     slope2 = popt[-1]
     scale = popt[0]
-    break_e = popt[1] 
+    break_e = popt[1]
     return scale, slope1, slope2, break_e
